c7bc253cc45aadc59627b948b1f407244ea5bae1
Sina Karvandi <SinaKarvandi@users.noreply.github.com>
2017-10-31 16:01:03 +0330

    Add files via upload

---
+++ Plugin/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin
+{
+    public class Class1
+    {
+        public Class1(string Arg1)
+        {
+            //Make program ready for first usage
+            System.Windows.Forms.MessageBox.Show("Constructor Invoked !" + Arg1);
+
+        }
+        public static string MyPluginFunction1(int a, int b)
+        {
+            System.Windows.Forms.MessageBox.Show("I'm here in MyPluginFunction.(Args:" + a + "-" + b + ")");
+            // Do what to want to do as a plugin
+            return "Successful";
+        }
+
+        public static string MyPluginFunction2()
+        {
+            System.Windows.Forms.MessageBox.Show("I'm here in MyPluginFunction (without arg).");
+            // Do what to want to do as a plugin
+            return "Successful";
+        }
+    }
+}
---
+++ UsePlugin/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsePlugin
+{
+    class Program
+    { 
+        static object InvokeAssemblyWithArgumant(string Path, string MethodName, object[] argumantToMethod, object[] ArgumantsToContructor = null)
+        {
+            object ret = null;
+            System.Reflection.Assembly myDllAssembly =
+            System.Reflection.Assembly.LoadFile(Path);
+            if (ArgumantsToContructor == null)
+            {
+                foreach (Type item in myDllAssembly.GetTypes())
+                {
+                    ret = item.GetMethod(MethodName).Invoke(Activator.CreateInstance(item), argumantToMethod);
+                }
+            }
+            else
+            {
+                foreach (Type item in myDllAssembly.GetTypes())
+                {
+                     ret = item.GetMethod(MethodName).Invoke(Activator.CreateInstance(item, ArgumantsToContructor), argumantToMethod);
+                }
+            }
+            return ret;
+        }
+
+        static object InvokeAssemblyWithoutArgumant(string Path, string MethodName, object[] ArgumantsToContructor = null)
+        {
+            object ret = null;
+            System.Reflection.Assembly myDllAssembly =
+            System.Reflection.Assembly.LoadFile(Path);
+            if (ArgumantsToContructor == null)
+            {
+                foreach (Type item in myDllAssembly.GetTypes())
+                {
+                    ret = item.GetMethod(MethodName).Invoke(Activator.CreateInstance(item), null);
+                }
+            }
+            else
+            {
+                foreach (Type item in myDllAssembly.GetTypes())
+                {
+                    ret = item.GetMethod(MethodName).Invoke(Activator.CreateInstance(item, ArgumantsToContructor), null);
+                }
+            }
+            return ret;
+        }
+        static void Main(string[] args)
+        {       
+            
+            //dll path (Plugin Path)
+            string PathToPlugin = Environment.CurrentDirectory + "\\Plugin.dll"; // it's extension can be anything !
+            // you can also load assembly from a base64 string.
+            InvokeAssemblyWithArgumant(PathToPlugin, "MyPluginFunction1",new object[] { 1, 2 }, new object[] { "Sample Arg to constructor" });
+            InvokeAssemblyWithoutArgumant(PathToPlugin, "MyPluginFunction2", new object[] { "Sample Arg to constructor" });
+
+        }
+    }
+}
